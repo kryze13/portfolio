f191069b4f7366338cb89966f4a9e91b9d4cf725
Dennis Hohl <dennis.hohl@pm.me>
2025-06-19 15:04:23 +0200

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-const App = () => {
-  return (
-   <main>
-    <div>
-        <h1>Welcome to my portfolio</h1>
-    </div>
-   </main>
-  );
-}
-export default App; 
+import Testimonials from "./sections/Testimonials";
+import Footer from "./sections/Footer";
+import Contact from "./sections/Contact";
+import TechStack from "./sections/TechStack";
+import Experience from "./sections/Experience";
+import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import LogoShowcase from "./sections/LogoShowcase";
+import FeatureCards from "./sections/FeatureCards";
+import Navbar from "./components/NavBar";
+
+const App = () => (
+  <>
+    <Navbar />
+    <Hero />
+    <ShowcaseSection />
+    <LogoShowcase />
+    <FeatureCards />
+    <Experience />
+    <TechStack />
+    <Testimonials />
+    <Contact />
+    <Footer />
+  </>
+);
+
+export default App;
